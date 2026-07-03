f33013bf61aeaec405aef65e0b441c4a61058d24
Will Maier <189851+whilp@users.noreply.github.com>
2026-07-03 15:34:19 -0700

    lua/unix: add annotation-coverage ratchet for definitions.lua (#127)

--- tool/lua/BUILD.mk
+++ tool/lua/BUILD.mk
@@ -142,6 +142,10 @@ o/$(MODE)/tool/lua/test_fetch_unix_proxy.ok: o/$(MODE)/tool/lua/lua.dbg tool/lua
 	$< tool/lua/test_fetch_unix_proxy.lua
 	@touch $@
 
+o/$(MODE)/tool/lua/test_definitions_coverage.ok: o/$(MODE)/tool/lua/lua.dbg tool/lua/test_definitions_coverage.lua third_party/lua/lunix.c tool/net/definitions.lua
+	$< tool/lua/test_definitions_coverage.lua
+	@touch $@
+
 TOOL_LUA_TESTS =							\
 	o/$(MODE)/tool/lua/test_cosmo.ok				\
 	o/$(MODE)/tool/lua/test_getopt.ok				\
@@ -152,7 +156,8 @@ TOOL_LUA_TESTS =							\
 	o/$(MODE)/tool/lua/test_zip_security.ok				\
 	o/$(MODE)/tool/lua/test_unix_proc.ok				\
 	o/$(MODE)/tool/lua/test_isatty.ok				\
-	o/$(MODE)/tool/lua/test_fetch_unix_proxy.ok
+	o/$(MODE)/tool/lua/test_fetch_unix_proxy.ok			\
+	o/$(MODE)/tool/lua/test_definitions_coverage.ok
 
 .PHONY: o/$(MODE)/tool/lua
 o/$(MODE)/tool/lua:							\
